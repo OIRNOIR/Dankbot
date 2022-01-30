e852be93370084bd7d40db2faa76055508938e0e
OIRNOIR <oirnoir@outlook.com>
2022-01-30 01:10:14 -0800

    Presence is no longer inefficient

--- events/ready.js
+++ events/ready.js
@@ -1,14 +1,5 @@
 "use strict";
 
 module.exports = async (instance) => {
-  instance.clientValid = true;
-  instance.client.sweepMessages(300);
-  instance.client.user.setPresence({status: instance.config.status, activities: [{name: instance.config.statusMessage.replace("{SERVERCOUNT}", instance.client.guilds.cache.size), type: instance.config.statusType}]});
   console.log("Logged in as " + instance.client.user.tag);
-  const interval = setInterval(() => {
-    if (instance.clientValid == false) return clearInterval(interval);
-    instance.client.sweepMessages(300);
-    instance.client.user.setPresence({status: instance.config.status, activities: [{name: instance.config.statusMessage.replace("{SERVERCOUNT}", instance.client.guilds.cache.size), type: instance.config.statusType}]});
-  }, 300000);
-  interval.unref();
 }
--- index.js
+++ index.js
@@ -4,28 +4,39 @@
 // Made with the previous code of Cinnamon
 // by OIRNOIR#0032 | https://github.com/OIRNOIR
 
-function requireUncached(module) {
-  delete require.cache[require.resolve(module)];
-  return require(module);
-}
-
 const fs = require('fs');
 const Discord = require('discord.js');
-const config = requireUncached('./config');
-let clientValid = false;
+const config = require('./config');
 
-const thisInstance = {client: new Discord.Client({partials: ['CHANNEL'], intents: ['GUILDS', 'GUILD_MESSAGES', 'DIRECT_MESSAGES']}), config, commands: new Discord.Collection(), functions: {}, clientValid};
+const thisInstance = {
+  client: new Discord.Client({
+    partials: ['CHANNEL'],
+    intents: ['GUILDS', 'GUILD_MESSAGES', 'DIRECT_MESSAGES'],
+    presence: {
+      status: instance.config.status,
+      activities: [
+        {
+          name: instance.config.statusMessage,
+          type: instance.config.statusType
+        }
+      ]
+    }
+  }),
+  config,
+  commands: new Discord.Collection(),
+  functions: {}
+};
 for (const dir of fs.readdirSync(__dirname + "/commands").filter(d => !d.startsWith("."))){
   const commands = fs.readdirSync(__dirname + "/commands/" + String(dir)).filter(file => file.endsWith('.js'));
   for (const commandFile of commands){
-    const command = requireUncached(`./commands/${dir}/${commandFile}`);
+    const command = require(`./commands/${dir}/${commandFile}`);
     thisInstance.commands.set(command.name.toLowerCase(), command);
   }
 }
 
 const events = fs.readdirSync(__dirname + '/events').filter(file => file.endsWith('js'));
 for (const eventFile of events){
-  const event = requireUncached(`${__dirname}/events/${eventFile}`);
+  const event = require(`${__dirname}/events/${eventFile}`);
   thisInstance.client.on(eventFile.split('.')[0], event.bind(null, thisInstance));
 }
 
